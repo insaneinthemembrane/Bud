455d4fed981bdc35ea4f7fb717a763691e4509de
yalta1945 <yalta1945@linuxmail.org>
2018-01-18 00:45:15 +0100

    Bud

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Bud-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BudURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
